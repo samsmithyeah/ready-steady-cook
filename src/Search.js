c095017e21b22a67d93522304b4d4a258fd02428
Sam <sam.smith@red-badger.com>
2021-02-23 15:21:35 +0000

    Improve progress component

--- src/Search.js
+++ src/Search.js
@@ -3,19 +3,17 @@ import SendIcon from '@material-ui/icons/Send';
 
 export default function Search(props) {
 
-  const { history, inputRef, setResults, searchTerm, setSearchTerm, setIsLoading, classes, setActiveStep } = props
+  const { history, inputRef, setResults, searchTerm, setSearchTerm, setIsLoading, classes } = props
   const { REACT_APP_APP_ID, REACT_APP_APP_KEY } = process.env;
 
   function handleSearch(event) {
     event.preventDefault();
-    console.log(process.env)
     setIsLoading(true)
     fetch(`https://api.edamam.com/search?q=${searchTerm}&app_id=${REACT_APP_APP_ID}&app_key=${REACT_APP_APP_KEY}&to=100`)
       .then(response => response.json())
       .then(data => {
         setResults(data.hits);
         setIsLoading(false)
-        setActiveStep(1)
         history.push('/filters')
       })
       .catch(err => {
