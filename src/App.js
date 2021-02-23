0bd94ea6fd459b4692b7f2d2802b68d0eb4abc05
Sam <sam.smith@red-badger.com>
2021-02-23 14:49:29 +0000

    Add progress component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
 import Results from './Results.js'
 import Search from './Search.js'
 import Filter from './Filter.js'
+import Progress from './Progress.js'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,6 +26,7 @@ export default function App() {
   const [filters, setFilters] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [activeStep, setActiveStep] = useState(0)
   const inputRef = useRef();
   const history = useHistory();
   const location = useLocation();
@@ -36,12 +38,11 @@ export default function App() {
   }, [location]);
 
 
-  
-
   function handleRestartClick() {
     setSearchTerm("")
     setFilters([])
     setFilteredResults([])
+    setActiveStep(0)
     history.push("/")
   }
 
@@ -50,12 +51,13 @@ export default function App() {
     <>
       <div className={classes.root}>
         <Box>
+          <Progress activeStep={activeStep}/>
           <Switch>
             <Route exact path="/">
-              {isLoading ? <CircularProgress /> : <Search history={history} inputRef={inputRef} searchTerm={searchTerm} setSearchTerm={setSearchTerm} setResults={setResults} setIsLoading={setIsLoading} classes={classes} />}
+              {isLoading ? <CircularProgress /> : <Search setActiveStep={setActiveStep} history={history} inputRef={inputRef} searchTerm={searchTerm} setSearchTerm={setSearchTerm} setResults={setResults} setIsLoading={setIsLoading} classes={classes} />}
             </Route>
             <Route exact path="/filters"> 
-              <Filter filters={filters} history={history} results={results} searchTerm={searchTerm} inputRef={inputRef} handleRestartClick={handleRestartClick} setFilters={setFilters} setFilteredResults={setFilteredResults}/>
+              <Filter setActiveStep={setActiveStep} filters={filters} history={history} results={results} searchTerm={searchTerm} inputRef={inputRef} handleRestartClick={handleRestartClick} setFilters={setFilters} setFilteredResults={setFilteredResults}/>
             </Route>
             <Route exact path="/results">
               <Results results={results} filters={filters} searchTerm={searchTerm} filteredResults={filteredResults} handleRestartClick={handleRestartClick}/>
