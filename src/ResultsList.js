4b9273810d699ccb10fe3b7c87a916eb9be3f124
Sam <sam.smith@red-badger.com>
2021-02-24 12:30:01 +0000

    Remove some stuff

--- src/ResultsList.js
+++ src/ResultsList.js
@@ -1,21 +1,7 @@
 import { Container, Link, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default function ResultsList(props) {
-  const classes = useStyles();
-  const { results } = props;
+  const { results, classes } = props;
   return (
     <Container className={classes.root}>
       <List className={classes.root}>
